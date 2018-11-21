25796f0c2f2729baefb4dfd2ac61d2559ed9d9ff
Edward Calver <hx010973@reading.ac.uk>
2018-11-21 21:16:35 +0000

    Display plots of each of the three solutions

--- src/plot.py
+++ src/plot.py
@@ -2,18 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import animation
 from solvers import solve
+from tests import analytic
 
 #Main plotting routine
 def plot_solution(solution):
 
 	fig=plt.figure()
-	ax=plt.axes(xlim=(-1,1),ylim=(-1,1))
+	ax=plt.axes(xlim=(-1,1),ylim=(0,1))
 	ax.set_aspect("equal")
 	line,=ax.plot([],[],lw=1)
 	line2,=ax.plot([],[],lw=1)
 	
 	def animate(i):
-		for i in range(1):
+		for i in range(50):
 			(t,x,u)=next(solution)
 		exact=np.array(list(map(lambda xn:analytic(xn,t),x)))
 		line.set_data(x,exact)
