b74a4e68c3e3eef60aeaa7a41c4b7baf72f9b7d3
Edward Calver <hx010973@reading.ac.uk>
2018-11-21 22:49:09 +0000

    Comment/clean up tests.py

--- src/tests.py
+++ src/tests.py
@@ -1,26 +1,40 @@
 import numpy as np
+
 from solvers import g,lax_wendroff,solve
 from analytic import analytic
 
 
+"""
+Initial conditions for the test cases we will consider - one smooth and one discontinous
+
+These functions take as input an array of x coordinates and return the initial values
+of the fluid height and discharge at those points
+"""
 def dam_break(x):
 	return np.stack([np.where(x<=0,1.0,0.5),np.zeros_like(x)])
 
 def hump(x):
 	return np.stack([np.where(np.abs(x)<=0.5,0.5+0.1*(1+np.cos(2.0*np.pi*x)),0.5),np.zeros_like(x)])
 
-
+"""
+Compute a numerical quadrature by the midpoint rule.
+"""
 def integrate(u,dx):
 	return sum(dx*u)
 	
-
-def l2_norm(x,u,domain):
-	(start,end)=domain
-	dx=x[1]-x[0]
-	u=np.where(np.logical_and(x>start,x<end),u,0.0)
+"""
+Compute the L2 norm
+"""
+def l2_norm(u,dx):
 	return np.sqrt(integrate(u*u,dx))
 
+"""
+Test the convergence rate of a list of solvers on the dam break case
 
+This function computes the numerical solution with each solver for a number of different
+values of dx and dt such that the Courant number is constant. The L2 norms of the errors
+are written to the file convergence.txt
+"""
 def do_convergence_test(solvers):
 	print("Running convergence test")
 	time=0.2
@@ -42,19 +56,26 @@ def do_convergence_test(solvers):
 			u=[]
 			for i in range(num_timesteps+1):
 				(t,x,u)=next(solution)
-			f.write(" %f"%l2_norm(x,u[0]-analytic(x,time),(-1,1)))
+			f.write(" %f"%l2_norm(np.where(np.logical_and(x>-1,x<1),u[0]-analytic(x,time),0),dx))
 		f.write("\n");
 		print("Computed errors for dx=%f"%dx)
 	f.close();
 	print("Output written to convergence.txt")
 
+"""
+Test the convergence rate of a list of solvers on the smooth initial condition
+
+Since no analytic solution is availabe in this case, the L2 norms are estimated using a
+numerical solution computed with a very high resolution. The results are written to the
+file convergence_smooth.txt
+"""
 def do_smooth_convergence_test(solvers):
 	print("Running smooth convergence test")
 
 	#Compute high resolution solution
 	time=0.05
 	domain=(-1,1)
-	courant=16.0
+	courant=4.0
 	high_res_num_cells=21870*3
 	high_res_num_timesteps=int(courant*high_res_num_cells*time/(domain[1]-domain[0]))
 	high_res_dt=time/high_res_num_timesteps
@@ -84,13 +105,19 @@ def do_smooth_convergence_test(solvers):
 			u=[]
 			for i in range(num_timesteps+1):
 				(t,x,u)=next(solution)
-			f.write(" %f"%l2_norm(x,u[0]-downsampled_high_res_h,(-1,1)))
+			f.write(" %f"%l2_norm(u[0]-downsampled_high_res_h,dx))
 		f.write("\n");
 		print("Computed errors for dx=%f"%dx)
 	f.close();
 	print("Output written to convergence_smooth.txt")
 
+"""
+Test how well the numerical solvers preserve conserved quantities
 
+This function computes the solution of the cosine bell problem with each 
+numerical scheme, and computes values of the mass, momentum, and energy at
+ each time step. The results are written to the file conservation.txt
+"""
 def do_conservation_test(solvers):
 	print("Running conservation test")
 	domain=(-1,1)
@@ -112,8 +139,19 @@ def do_conservation_test(solvers):
 	f.close();
 	print("Output written to conservation.txt")
 
+"""
+Compute the total variation of u
+"""
 def total_variation(u):
 	return sum(np.abs(u[1:]-u[0:-1]))
+
+"""
+Test whether the numerical methods introduce spurious oscillations
+
+This function computes the numerical solution of the dam break problem with each
+numerical scheme, and computes the total variation at each time step. The results
+are written to the file total_variation.txt
+"""
 
 def do_total_variation_test(solvers):
 	print("Running total variation test")
